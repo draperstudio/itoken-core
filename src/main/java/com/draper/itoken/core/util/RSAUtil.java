995d4a3dc4439515417ee83636959fe799c3ade4
Draper_HXY <draper_hxy@163.com>
2019-03-23 23:47:02 +0800

    refactor RSAUtil

--- src/main/java/com/draper/itoken/core/util/RSAUtil.java
+++ src/main/java/com/draper/itoken/core/util/RSAUtil.java
@@ -1,11 +1,18 @@
 package com.draper.itoken.core.util;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 
+import javax.crypto.SecretKey;
 import java.io.*;
-import java.security.KeyPair;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 
 /**
@@ -27,6 +34,32 @@ public class RSAUtil {
         return publicBytes;
     }
 
+    public static PrivateKey getPrivateKey(byte[] privateKeyBytes) {
+        try {
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+            KeyFactory keyf = KeyFactory.getInstance("RSA");
+            return keyf.generatePrivate(keySpec);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static PublicKey getPublicKey(byte[] publicKeyBytes){
+        try {
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
+            KeyFactory keyf = KeyFactory.getInstance("RSA");
+            return keyf.generatePublic(keySpec);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public static byte[] getPrivateBytes() {
         if (privateKey == null) {
             privateKey = readPrivateKey();
@@ -35,52 +68,36 @@ public class RSAUtil {
     }
 
     private static byte[] readPublicKey() {
+        byte[] bytes = null;
         try {
             File file = new File("publicKey.key");
             // 文件不存在则会生成一对秘钥
             if (!file.exists()) {
                 generateRSAKeyPair();
             }
-            FileReader fileReader = new FileReader("publicKey.key");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String str = null;
-            String result = null;
-            while ((str = bufferedReader.readLine()) != null) {
-                if (result == null) {
-                    result = str;
-                } else {
-                    result = result + str;
-                }
-            }
-            return result.getBytes();
+            FileInputStream fileInputStream = new FileInputStream(file);
+            bytes = new byte[fileInputStream.available()];
+            fileInputStream.read(bytes);
         } catch (Throwable t) {
             t.printStackTrace();
         }
-        return null;
+        return bytes;
     }
 
     private static byte[] readPrivateKey() {
+        byte[] bytes = null;
         try {
             File file = new File("privateKey.key");
             if (!file.exists()) {
                 generateRSAKeyPair();
             }
-            FileReader fileReader = new FileReader("privateKey.key");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String str = null;
-            String result = null;
-            while ((str = bufferedReader.readLine()) != null) {
-                if (result == null) {
-                    result = str;
-                } else {
-                    result = result + str;
-                }
-            }
-            return result.getBytes();
+            FileInputStream fileInputStream = new FileInputStream(file);
+            bytes = new byte[fileInputStream.available()];
+            fileInputStream.read(bytes);
         } catch (Throwable t) {
             t.printStackTrace();
         }
-        return null;
+        return bytes;
     }
 
     private static void generateRSAKeyPair() {
@@ -88,18 +105,14 @@ public class RSAUtil {
 
         try {
             byte[] publicKeyBytes = keyPair.getPublic().getEncoded();
-            FileWriter fileWriter = new FileWriter("publicKey.key");
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write(new String(publicKeyBytes));
-            bufferedWriter.flush();
-            bufferedWriter.close();
+            FileOutputStream fileOutputStream = new FileOutputStream("publicKey.key");
+            fileOutputStream.write(publicKeyBytes);
+            fileOutputStream.close();
 
             byte[] privateKeyBytes = keyPair.getPrivate().getEncoded();
-            fileWriter = new FileWriter("privateKey.key");
-            bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write(new String(privateKeyBytes));
-            bufferedWriter.flush();
-            bufferedWriter.close();
+            fileOutputStream = new FileOutputStream("privateKey.key");
+            fileOutputStream.write(privateKeyBytes);
+            fileOutputStream.close();
 
         } catch (Throwable t) {
             t.printStackTrace();
