7b72a3bb6357dbe8daf86da435dc936d40c8c20b
Draper_HXY <draper_hxy@163.com>
2019-03-23 23:48:51 +0800

    style

--- src/main/java/com/draper/itoken/core/util/RSAUtil.java
+++ src/main/java/com/draper/itoken/core/util/RSAUtil.java
@@ -1,14 +1,10 @@
 package com.draper.itoken.core.util;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 
-import javax.crypto.SecretKey;
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
@@ -118,6 +114,5 @@ public class RSAUtil {
             t.printStackTrace();
         }
     }
-
 
 }
